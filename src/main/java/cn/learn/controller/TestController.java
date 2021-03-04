6e2b89f05c903b2fc7a195a8c58c1640fa48ad4b
陈智云 <zhiyun.chen@autozi.com>
2021-03-04 10:12:08 +0800

    Initial commit

---
+++ src/main/java/cn/learn/controller/TestController.java
@@ -0,0 +1,12 @@
+package cn.learn.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @RequestMapping("/test")
+    public String test(){
+        return "Spring Boot Demo";
+    }
+}
